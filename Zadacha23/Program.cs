cd029493ec7046425befd3b3453d73899ff3bba2
Natalia <gurova_nata22@mail.ru>
2022-08-01 05:02:04 +0300

    Add Zadacha23

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,18 @@
+﻿void Zadacha23()
+{
+    //Напишите программу, которая принимает на вход число (N) и выдаёт
+    //таблицу кубов чисел от 1 до N.
+
+    Console.Write("Введите число: ");
+    int number_end = Convert.ToInt32(Console.ReadLine());
+    int number_start = 1;
+
+    while (number_start <= number_end)
+    {
+        double result = Math.Pow(number_start, 3);
+        Console.Write(result + " ");
+        number_start++;
+    }
+}
+
+Zadacha23();
